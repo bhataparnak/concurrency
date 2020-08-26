87bfe14a1560295e840393fd17b4b19072b75952
Aparna Bhat <bhat_aparna1@yahoo.co.in>
2020-08-25 19:00:53 -0500

    Update officehours.c

--- officehours.c
+++ officehours.c
@@ -1,9 +1,5 @@
 
-/*Student ID: 1001255079
-  Name: Aparna Krishna Bhat*/
 
-// Copyright (c) 2020 Trevor Bakker
-//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
